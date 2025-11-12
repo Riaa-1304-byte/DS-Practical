5b2e785e003890e47ffc6f513a366d92d072e45e
Riaa-1304-byte <riyathakur121312@gmail.com>
2025-11-12 20:19:06 +0530

    Create 2_sum.c

---
+++ 2_sum.c
@@ -0,0 +1,13 @@
+@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b,sum;
+    printf("Enter 1st number-> ");
+    scanf("%d",&a);
+
+    printf("Enter 2nd number-> ");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("Sum=%d",sum);
+    return 0;
+}
